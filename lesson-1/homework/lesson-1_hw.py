608337a390283e1dcfac5ea4871bbd316bf35a46
Bobur Mirzo <babur565@gmail.com>
2025-06-07 17:15:19 +0500

    Create lesson-1_hw.py

---
+++ lesson-1/homework/lesson-1_hw.py
@@ -0,0 +1,31 @@
+#Task 1 Given a side of square. Find its perimeter and area.
+a = 10 #square side
+p = 4 * a #Perimeter
+s = a * a # Square
+print(p)
+print(s)
+
+#Task 2 Given diameter of circle. Find its length.
+d = 10 #diameter of a circle
+l = 2 * 3.14 # l = P * d
+print(l)
+
+#Task 3 Given two numbers a and b. Find their mean.
+a = 3
+b = 4
+m = (a + b)/2
+print(m)
+
+#Task 4 Given two numbers a and b. Find their sum, product and square of each number.
+a = 5
+b = 6
+
+sum = a + b
+product = a * b
+a_square = a ** 2
+b_square = b ** 2
+
+print(sum)
+print(product)
+print(a_square)
+print(b_square)
