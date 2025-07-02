068fb3fce9a63b7098612f6a2f22adf462d088c1
Bobur Mirzo <babur565@gmail.com>
2025-07-02 17:30:53 +0500

    Update lesson-8_hw.py

--- lesson-8/homework/lesson-8_hw.py
+++ lesson-8/homework/lesson-8_hw.py
@@ -84,3 +84,115 @@ try:
     my_list.upper()  # list has no method 'upper'
 except AttributeError:
     print('There is no such method')
+
+# 2nd part File input
+
+#1.Write a Python program to read an entire text file.
+with open('/Users/boburmirzobobojonov/Courses/python_hw/hw_8/animals.txt') as f:
+    data = f.read()
+print(data)
+
+#2.Write a Python program to read first n lines of a file.
+n = int(input('Enter number of lines to read: '))
+with open('/Users/boburmirzobobojonov/Courses/python_hw/hw_8/animals.txt') as f:
+    for i in range(n):
+        data = f.readline()
+        if data == '': #meaning end of file
+            break
+        print(data.strip()) 
+
+#3.Write a Python program to append text to a file and display the text.
+
+text = 'Elephant'
+
+with open('/Users/boburmirzobobojonov/Courses/python_hw/hw_8/animals.txt', 'a') as f:
+    data = f.write(text + '\n')
+with open('/Users/boburmirzobobojonov/Courses/python_hw/hw_8/animals.txt') as f:
+    data = f.read()
+    print(data)
+
+#4.Write a Python program to read last n lines of a file.
+
+n = int(input('Enter number of lines to read: '))
+with open('/Users/boburmirzobobojonov/Courses/python_hw/hw_8/animals.txt') as f:
+      data = f.readlines()
+for i in data[-n:]:
+      print(i.strip())
+
+#5.Write a Python program to read a file line by line and store it into a list.
+animals = []
+with open('/Users/boburmirzobobojonov/Courses/python_hw/hw_8/animals.txt') as f:
+    for line in f:
+        animals.append(line.strip())
+print(animals)
+
+#6.Write a Python program to read a file line by line and store it into a variable.
+animals = ""
+
+with open('/Users/boburmirzobobojonov/Courses/python_hw/hw_8/animals.txt') as f:
+    for line in f:
+        animals += line
+print(animals)
+
+#7.Write a Python program to read a file line by line and store it into an array.
+animals = []
+with open('/Users/boburmirzobobojonov/Courses/python_hw/hw_8/animals.txt') as f:
+    for line in f:
+        animals.append(line.strip())
+print(animals)
+
+
+#8.Write a Python program to find the longest words.
+words = ['table', 'tank', 'experience', 'boring', 'book']
+longest = max(words, key=len)
+print(longest)
+
+#9.Write a Python program to count the number of lines in a text file.
+
+lines = 0
+
+with open('/Users/boburmirzobobojonov/Courses/python_hw/hw_8/animals.txt') as f:
+    for line in f:
+        lines += 1
+print('Total number of lines: ' + lines)
+
+#10.Write a Python program to count the frequency of words in a file.
+# import string
+
+word_counts = {}
+with open('/Users/boburmirzobobojonov/Courses/python_hw/hw_8/animals.txt') as f:
+    for line in f:
+        clean_line = line.translate(str.maketrans('','', string.punctuation)).lower()
+        words = clean_line.split()
+
+        for word in words:
+            if word in word_counts:
+                word_counts[word] += 1
+            else:
+                word_counts[word] = 1
+for word, count in word_counts.items():
+    print(f"{word}: {count}")
+
+#11. Write a Python program to get the file size of a plain file.
+import os
+file_size = os.path.getsize('/Users/boburmirzobobojonov/Courses/python_hw/hw_8/animals.txt')
+print('File size:', file_size, 'bytes')
+
+#12. Write a Python program to write a list to a file.
+
+fishes = ['catfish', 'carp', 'goldfish', 'salmon', 'tuna']
+
+with open('/Users/boburmirzobobojonov/Courses/python_hw/hw_8/animals.txt','w') as f:
+    for fish in fishes:
+       data = f.write(fish + '\n')
+
+#13. Write a Python program to copy the contents of a file to another file.
+with open('/Users/boburmirzobobojonov/Courses/python_hw/hw_8/animals.txt') as animals, open('/Users/boburmirzobobojonov/Courses/python_hw/hw_8/wild_animals.txt', 'a') as wild_animals:
+    for line in animals:
+        wild_animals.write(line)
+
+#14. Write a Python program to combine each line from the first file with the corresponding line in the second file.
+with open('/Users/boburmirzobobojonov/Courses/python_hw/hw_8/animals.txt') as fishes, open('/Users/boburmirzobobojonov/Courses/python_hw/hw_8/wild_animals.txt') as wild_animals:
+    for fish, wild_animal in zip(fishes, wild_animals):
+        combined = fish.strip() + '--' + wild_animal.strip()
+        print(combined)
